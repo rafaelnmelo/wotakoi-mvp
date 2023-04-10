d5fda73ae41334da622e86218a9090d9edb0f292
Rafael Mobicare <rafael.melo@mobicare.com.br>
2023-04-10 17:44:55 -0300

    Iniciando estudos de teste unitário

---
+++ WotakoiTests/StringUtilsSpec.swift
@@ -0,0 +1,28 @@
+import XCTest
+@testable import Wotakoi
+
+class StringUtilsSpec: XCTestCase {
+
+    func testIsValidEmail() {
+        let email = "Wotakoi@gmail.com"
+        let value = email.isValidEmail()
+        
+        XCTAssertTrue(value, "\n\nNão é um email válido\n\n")
+        XCTAssert(value == true)
+    }
+    
+    func testRemoveWhiteSpace() {
+        let text = " olá mundo "
+        let newText = text.removeWhitespace()
+        
+        XCTAssertTrue(newText == "olámundo", "\n\nNão removeu os espaços\n\n")
+    }
+    
+    func testReplace() {
+        let text = "Menino"
+        let genderSwap = text.replace(string: "o", replacement: "a")
+        
+        XCTAssertTrue(genderSwap == "Menine", "\n\nTroca não realizada\n\n")
+    }
+
+}
