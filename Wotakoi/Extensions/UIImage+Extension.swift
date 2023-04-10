6e717efecdc06907cb9d80e66dd62060dd673c91
Rafael Mobicare <rafael.melo@mobicare.com.br>
2023-04-10 10:59:19 -0300

    UIImage Extension

---
+++ Wotakoi/Extensions/UIImage+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  UIImage+Extension.swift
+//  Wotakoi
+//
+//  Created by Rafael Melo on 10/04/23.
+//  Copyright © 2023 Rafael Nunes. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    static let house = UIImage(named: "house")
+    static let logo = UIImage(named: "logo")
+    static let star = UIImage(named: "star")
+}
