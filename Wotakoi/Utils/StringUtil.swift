57f2d7248b591561c7affec9053111d938d1dbe6
Rafael Mobicare <rafael.melo@mobicare.com.br>
2023-04-11 10:01:45 -0300

    Ajuste referencias

---
+++ Wotakoi/Utils/StringUtil.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+extension String {
+    
+    func removeWhitespace() -> String {
+        return self.replace(string: " ", replacement: "")
+    }
+    
+    func replace(string:String, replacement:String) -> String {
+        return self.replacingOccurrences(of: string, with: replacement, options: .literal, range: nil)
+    }
+    
+    func isValidEmail() -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: self)
+    }
+}
